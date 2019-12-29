782ab951c0898554eee64275b17c677e5d4317e1
Baichenghui <2696437433@qq.com>
2019-12-29 20:02:22 +0800

    init:CoreTextDemo

---
+++ CoreTextDemo/CoreTextDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CoreTextDemo
+//
+//  Created by 白成慧&瑞瑞 on 2019/12/29.
+//  Copyright © 2019 hh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
