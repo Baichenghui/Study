6a22be0a1b2527ad321e3c8aa7f82673d79c1421
baichenghui88888@gmail.com <baichenghui88888@gmail.com>
2020-09-25 16:26:27 +0800

    add:learn FlextLayout

---
+++ FlextLayoutDemo/FlextLayoutDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlextLayoutDemo
+//
+//  Created by tianxi on 2020/9/24.
+//  Copyright © 2020 hh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
